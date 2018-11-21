d80e3a604dd3d1f31ec1b2d40d46da24ff544283
Douglas Blank <doug.blank@gmail.com>
2018-11-21 16:18:52 -0500

    Better pytorch version

--- pytorch/comet-pytorch-mnist-example.py
+++ pytorch/comet-pytorch-mnist-example.py
@@ -1,10 +1,10 @@
+from comet_ml import Experiment
+
 import torch
 import torch.nn as nn
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
-from comet_ml import Experiment
-import os
 
 hyper_params = {
     "sequence_length": 28,
@@ -13,9 +13,14 @@ hyper_params = {
     "num_layers": 2,
     "num_classes": 10,
     "batch_size": 100,
-    "num_epochs": 2,
+    "num_epochs": 3,
     "learning_rate": 0.01
 }
+
+
+experiment = Experiment(project_name="pytorch")
+experiment.log_multiple_params(hyper_params)
+
 
 # MNIST Dataset
 train_dataset = dsets.MNIST(root='./data/',
@@ -43,16 +48,13 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size,
-                            num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         # Set initial states
-        h0 = Variable(torch.zeros(self.num_layers,
-                                  x.size(0), self.hidden_size))
-        c0 = Variable(torch.zeros(self.num_layers,
-                                  x.size(0), self.hidden_size))
+        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
 
         # Forward propagate RNN
         out, _ = self.lstm(x, (h0, c0))
@@ -62,68 +64,63 @@ class RNN(nn.Module):
         return out
 
 
-# Setting the API key (saved as environment variable)
-experiment = Experiment(
-    #api_key="YOUR API KEY",
-    # or
-    api_key=os.environ.get("COMET_API_KEY"),
-    project_name='comet-examples')
-experiment.log_multiple_params(hyper_params)
 
-rnn = RNN(hyper_params['input_size'], hyper_params['hidden_size'],
-          hyper_params['num_layers'], hyper_params['num_classes'])
+rnn = RNN(hyper_params['input_size'],
+          hyper_params['hidden_size'],
+          hyper_params['num_layers'],
+          hyper_params['num_classes'])
 
 # Loss and Optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(
-    rnn.parameters(), lr=hyper_params['learning_rate'])
+optimizer = torch.optim.Adam(rnn.parameters(), lr=hyper_params['learning_rate'])
 
 # Train the Model
 
-for epoch in range(hyper_params['num_epochs']):
+with experiment.train():
+
+    step = 0
+    for epoch in range(hyper_params['num_epochs']):
+        experiment.log_current_epoch(epoch)
+        correct = 0
+        total = 0
+        for i, (images, labels) in enumerate(train_loader):
+            images = Variable(images.view(-1, hyper_params['sequence_length'],
+                                          hyper_params['input_size']))
+            labels = Variable(labels)
+
+            # Forward + Backward + Optimize
+            optimizer.zero_grad()
+            outputs = rnn(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # Compute train accuracy
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += float((predicted == labels.data).sum())
+
+            # Log accuracy to Comet.ml
+            experiment.log_metric("accuracy", correct / total, step=step)
+            step += 1
+
+            if (i + 1) % 100 == 0:
+                print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
+                      % (epoch + 1, hyper_params['num_epochs'], i + 1,
+                         len(train_dataset) // hyper_params['batch_size'],
+                         loss.data.item()))
+with experiment.test():
+    # Test the Model
     correct = 0
     total = 0
-    experiment.log_current_epoch(epoch)
-    for i, (images, labels) in enumerate(train_loader):
-        images = Variable(
-            images.view(-1, hyper_params['sequence_length'], hyper_params['input_size']))
-        labels = Variable(labels)
-
-        # Forward + Backward + Optimize
-        optimizer.zero_grad()
+    for images, labels in test_loader:
+        images = Variable(images.view(-1, hyper_params['sequence_length'],
+                                      hyper_params['input_size']))
         outputs = rnn(images)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # Compute train accuracy
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
-        correct += (predicted == labels.data).sum()
+        correct += float((predicted == labels).sum())
 
-        print(correct.item())
-        print(total)
-
-        # Log to Comet.ml
-        experiment.set_step(i)
-        experiment.log_metric("loss", loss.item())
-        experiment.log_metric("accuracy", correct.item() / total)
-
-        if (i + 1) % 100 == 0:
-            print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
-                  % (epoch + 1, hyper_params['num_epochs'], i + 1, len(train_dataset) // hyper_params['batch_size'], loss.data[0]))
-
-# Test the Model
-correct = 0
-total = 0
-for images, labels in test_loader:
-    images = Variable(
-        images.view(-1, hyper_params['sequence_length'], hyper_params['input_size']))
-    outputs = rnn(images)
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
-
-experiment.log_metric("test_accuracy", 100 * correct / total)
-print('Test Accuracy of the model on the 10000 test images: %d %%' %
-      (100 * correct.item() / total))
+    experiment.log_metric("accuracy", correct / total)
+    print('Test Accuracy of the model on the 10000 test images: %d %%'
+          % (100 * correct / total))

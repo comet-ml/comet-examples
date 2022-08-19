a2d18f33c16059e777b4add435da8accd2a64da3
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2022-08-19 15:13:43 +0300

    CM-4179: Added support for network transport TAG identifier to mnist-dnn-rich.py

--- keras/mnist-dnn-rich.py
+++ keras/mnist-dnn-rich.py
@@ -2,15 +2,16 @@ from __future__ import print_function
 import logging
 import utils
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 from comet_ml import Experiment
+from comet_ml.utils import tag_experiment_with_transport_layer_identifier
 
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense, Dropout
-
 
 params = {
     'dropout': 0.2,
@@ -24,15 +25,14 @@ params = {
     'optimizer': 'adam'
 }
 
+
 def main():
-    
     mnist = tf.keras.datasets.mnist
-    
+
     num_classes = 10
 
     # the data, shuffled and split between train and test sets
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
 
     x_train = x_train.reshape(60000, 784)
     x_test = x_test.reshape(10000, 784)
@@ -47,27 +47,26 @@ def main():
     y_train = to_categorical(y_train, num_classes)
     y_test = to_categorical(y_test, num_classes)
 
-
     train(x_train, y_train, x_test, y_test)
 
+
 def build_model_graph(exp, input_shape=(784,)):
-    
     model = Sequential([
-      Flatten(input_shape=(784, )),
-      Dense(exp.get_parameter('layer-1-size'), activation='relu'),
-      Dense(exp.get_parameter('layer-2-size'), activation='relu'),
-      Dropout(exp.get_parameter('dropout')),
-      Dense(10)
+        Flatten(input_shape=(784,)),
+        Dense(exp.get_parameter('layer-1-size'), activation='relu'),
+        Dense(exp.get_parameter('layer-2-size'), activation='relu'),
+        Dropout(exp.get_parameter('dropout')),
+        Dense(10)
     ])
-    
+
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate=exp.get_parameter('initial-lr'),
         decay_steps=exp.get_parameter('decay-steps'),
         decay_rate=exp.get_parameter('decay-rate'))
 
     loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
-    
-    model.compile(optimizer='adam', 
+
+    model.compile(optimizer='adam',
                   loss=loss_fn,
                   metrics=['accuracy'])
 
@@ -75,15 +74,12 @@ def build_model_graph(exp, input_shape=(784,)):
 
 
 def train(x_train, y_train, x_test, y_test):
-    
-    exp = Experiment(api_key="API_KEY", project_name="PROJECT", workspace="WORKSPACE")
-    
-    # log custom hyperparameters
-    exp.log_parameters(params)
-    
+    exp = Experiment()
+    tag_experiment_with_transport_layer_identifier(exp)
 
-    
-    
+    # log custom hyper-parameters
+    exp.log_parameters(params)
+
     # Define model
     model = build_model_graph(exp)
 
@@ -98,7 +94,6 @@ def train(x_train, y_train, x_test, y_test):
     score = model.evaluate(x_test, y_test, verbose=0)
     logging.info("Score %s", score)
 
-    
     # Finalize model includes the following calls
     # exp.log_confusion_matrix()
     # exp.log_image()
@@ -106,7 +101,7 @@ def train(x_train, y_train, x_test, y_test):
     # exp.add_tag()
     # exp.log_model()
     utils.finalize_model(model, x_train, y_train, x_test, y_test, exp)
-    
+
 
 if __name__ == "__main__":
     main()

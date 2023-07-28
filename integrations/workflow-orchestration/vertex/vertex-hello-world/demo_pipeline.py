69815960ec02ffd940ff9bd265d6aa9ae1ba9578
Boris Feld <lothiraldan@gmail.com>
2023-07-28 18:13:29 +0200

    Update Vertex example for newer Comet SDK version

--- integrations/workflow-orchestration/vertex/vertex-hello-world/demo_pipeline.py
+++ integrations/workflow-orchestration/vertex/vertex-hello-world/demo_pipeline.py
@@ -20,16 +20,8 @@ def data_preprocessing(a: str = None, b: str = None) -> str:
     import time
 
     import comet_ml
-    import comet_ml.integration.vertex
-
-    pipeline_run_name = "{{$.pipeline_job_name}}"
-    pipeline_task_name = "{{$.pipeline_task_name}}"
-    pipeline_task_id = "{{$.pipeline_task_uuid}}"
 
     experiment = comet_ml.Experiment()
-    experiment = comet_ml.integration.vertex.initialize_comet_logger(
-        experiment, pipeline_run_name, pipeline_task_name, pipeline_task_id
-    )
 
     for i in range(60):
         experiment.log_metric("accuracy", math.log(i + random.random()))
@@ -45,16 +37,8 @@ def model_training(a: str = None, b: str = None) -> str:
     import time
 
     import comet_ml
-    import comet_ml.integration.vertex
 
     experiment = comet_ml.Experiment()
-    pipeline_run_name = "{{$.pipeline_job_name}}"
-    pipeline_task_name = "{{$.pipeline_task_name}}"
-    pipeline_task_id = "{{$.pipeline_task_uuid}}"
-
-    experiment = comet_ml.integration.vertex.initialize_comet_logger(
-        experiment, pipeline_run_name, pipeline_task_name, pipeline_task_id
-    )
 
     for i in range(60):
         experiment.log_metric("accuracy", math.log(i + random.random()))
@@ -70,16 +54,8 @@ def model_evaluation(a: str = None, b: str = None) -> str:
     import time
 
     import comet_ml
-    import comet_ml.integration.vertex
 
     experiment = comet_ml.Experiment()
-    pipeline_run_name = "{{$.pipeline_job_name}}"
-    pipeline_task_name = "{{$.pipeline_task_name}}"
-    pipeline_task_id = "{{$.pipeline_task_uuid}}"
-
-    experiment = comet_ml.integration.vertex.initialize_comet_logger(
-        experiment, pipeline_run_name, pipeline_task_name, pipeline_task_id
-    )
 
     for i in range(60):
         experiment.log_metric("accuracy", math.log(i + random.random()))
@@ -106,35 +82,20 @@ model_evaluation_op = kfp.components.create_component_from_func(
 def pipeline():
     import comet_ml.integration.vertex
 
-    comet_api_key = os.getenv("COMET_API_KEY")
-    comet_project_name = os.getenv("COMET_PROJECT_NAME", COMET_PROJECT_NAME)
-    comet_workspace = os.getenv("COMET_WORKSPACE")
-
-    comet_ml.integration.vertex.comet_logger_component(
+    logger = comet_ml.integration.vertex.CometVertexPipelineLogger(
         # api_key=XXX,
         project_name=COMET_PROJECT_NAME,
         # workspace=XXX
+        share_api_key_to_workers=True,
     )
 
-    def add_comet_env(task):
-        if comet_api_key:
-            task.container.set_env_variable("COMET_API_KEY", comet_api_key)
+    task_1 = logger.track_task(data_preprocessing_op("test"))
 
-        if comet_project_name:
-            task.container.set_env_variable("COMET_PROJECT_NAME", comet_project_name)
+    task_2 = logger.track_task(model_training_op(task_1.output))
 
-        if comet_workspace:
-            task.container.set_env_variable("COMET_WORKSPACE", comet_workspace)
+    task_3 = logger.track_task(model_training_op(task_1.output))
 
-        return task
-
-    task_1 = add_comet_env(data_preprocessing_op("test"))
-
-    task_2 = add_comet_env(model_training_op(task_1.output))
-
-    task_3 = add_comet_env(model_training_op(task_1.output))
-
-    _ = add_comet_env(model_evaluation_op(task_2.output, task_3.output))
+    _ = logger.track_task(model_evaluation_op(task_2.output, task_3.output))
 
 
 if __name__ == "__main__":
